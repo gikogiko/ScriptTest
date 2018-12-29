17877385dbb6296d278670a35b43b4583ded5bbc
blend8e <blend8e@gmail.com>
2018-12-29 14:00:22 +0900

    Unity Lesson4 課題提出
    課題：「配列を宣言して出力しましょう」

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //配列を出力する課題
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        //配列内の値を要素順に表示
+        for (int i=0;i<array.Length;i++) {
+            Debug.Log(array[i]);
+        }
+
+        //配列内の値を要素の逆順に表示
+        for (int i = array.Length-1; i >=0 ; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
